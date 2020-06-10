9c3863fa69a16207ba362d0dbfb113c5518658c1
Cristina <cristinaquinsan710@gmail.com>
2020-06-10 17:36:42 +0100

    starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
 
+/** Firebase Modules */
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { config } from './services/utils/firebase';
+
+/** Our Components */
+import { AppComponent } from './app.component';
 import { EventsComponent } from './components/events/events.component';
 import { EventComponent } from './components/events/event/event.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+/** Materials Modules */
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
-    EventComponent
+    EventComponent, 
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,7 +35,10 @@ import { EventComponent } from './components/events/event/event.component';
     AngularFireAnalyticsModule,
     AngularFireModule,
     AngularFireAuthModule,
-    FormsModule
+    FormsModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
 
 
