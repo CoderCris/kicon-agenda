7dc13b3743f3504c75bdbacc89840a421cdd6af3
CoderCris <crisamer.ortega@gmail.com>
2020-08-03 11:05:05 +0100

    saving work

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { db } from 'src/app/services/utils/firebase';
+import * as firebase from 'firebase';
+
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore'
+
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent implements OnInit {
+export class EventsComponent {
 
-  constructor() { }
+  public events: Observable<any[]>;
+  public cat: Observable<any[]>;
+  categoryValue$: boolean=false;
+  //@ViewChild(DialogComponent) modal: DialogComponent;
+  panelOpenState = false;
 
-  ngOnInit(): void {
+  constructor(private firestore: AngularFirestore) { 
+
+    this.events = firestore.collection('events').valueChanges({ idField: 'eventId'});
+    console.log('los eventos se cargan')
+
+  }
+
+  remove(id_event){
+    db.collection('events').doc(id_event).delete().then(function(){
+      console.log('Evento eliminado');
+    }).catch(function(error){
+      console.log('ERROR: algo falló en la eliminación');
+    });
   }
 
 }
