775272bba356e59d1798e98be6bec8b2c3668901
Cristina <cristinaquinsan710@gmail.com>
2020-08-10 10:36:52 +0100

    start 10.08.2020

---
+++ src/app/components/newPoll/newPoll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newpoll',
+  templateUrl: './newPoll.component.html',
+  styleUrls: ['./newPoll.component.css']
+})
+export class NewPollComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
